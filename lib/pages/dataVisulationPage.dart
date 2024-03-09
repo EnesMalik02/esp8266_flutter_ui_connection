368e52cfc047fe50de89e65dfcc0f724bb5ddfa9
Enes Malik Arı <89933069+EnesMalik02@users.noreply.github.com>
2024-03-09 21:09:22 +0300

    today what i learned 9/3

---
+++ lib/pages/dataVisulationPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class dataVisulationPage extends StatelessWidget {
+  const dataVisulationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
